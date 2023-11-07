bd4dd6bae4f0862a40f67df593eb2101c87a8f9c
Vijay Dahiphale <vijaydahiphale96@gmail.com>
2023-11-07 18:32:25 -0500

    setup project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('File content:');
+import * as fs from 'fs';
+
+const filePath = './inputFile/sample.txt'; // Replace with the path to your file
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+
+  console.log('File content:');
+  console.log(data);
+});
