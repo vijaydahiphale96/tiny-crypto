b24ebd13eb273281fc8c7816f7c04091b6442230
Vijay Dahiphale <vijaydahiphale96@gmail.com>
2023-11-10 19:34:12 -0500

    generated keys

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ const CHUNK_SIZE = 8; // 64 bits = 8 bytes
 const s: number[] = [14, 4, 11, 1, 7, 9, 12, 10, 13, 2, 0, 15, 8, 5, 3, 6];
 
 function ANUEncrypt(value: bigint): bigint {
-  const lower32Bits: bigint = value & BigInt(0xFFFFFFFF);
-  const upper32Bits: bigint = (value >> BigInt(32)) & BigInt(0xFFFFFFFF);
+  let lower32Bits: bigint = value & BigInt("0xFFFFFFFF");
+  let upper32Bits: bigint = (value >> BigInt(32)) & BigInt("0xFFFFFFFF");
 
   // const hexString: string = value.toString(16);
   // const hexE = BigInt("0xE");
@@ -39,8 +39,8 @@ function sBox(b: bigint): bigint {
 const s2: number[] = [10, 3, 9, 14, 1, 13, 15, 4, 12, 5, 7, 2, 6, 8, 0, 11];
 
 function ANUDecrypt(value: bigint): bigint {
-  const lower32Bits: bigint = value & BigInt(0xFFFFFFFF);
-  const upper32Bits: bigint = (value >> BigInt(32)) & BigInt(0xFFFFFFFF);
+  const lower32Bits: bigint = value & BigInt("0xFFFFFFFF");
+  const upper32Bits: bigint = (value >> BigInt(32)) & BigInt("0xFFFFFFFF");
   // const hexString: string = value.toString(16);
   // const hexE = BigInt("0xE");
   // console.log(hexE); // Output: 14n
@@ -108,8 +108,45 @@ const inputFile = './inputFile/sample.MOV';
 const encryptedFile = 'encrypted_video.MOV';
 const decryptedFile = 'decrypted_video.MOV';
 
+
+
 // Reading the input video file
 const videoBuffer = fs.readFileSync(inputFile);
+
+// generate keys
+
+const keysList= [];
+
+function generateKeys() {
+  const initialKeys = [BigInt(0), BigInt(0)];
+  for(let i = 0 ; i<25; i++) {
+
+    keysList.push(initialKeys);
+    console.log("Round - ", i);
+    console.log("Start - ", initialKeys[0].toString(16), initialKeys[1].toString(16));
+
+    const temp = initialKeys[1];
+    initialKeys[1]= ((temp<<BigInt(13)) | (initialKeys[0]>>BigInt(64-13))) & BigInt("0xFFFFFFFFFFFFFFFF");
+		initialKeys[0]= ((initialKeys[0]<<BigInt(13)) | (temp>>BigInt((64-13)))) & BigInt("0xFFFFFFFFFFFFFFFF");
+
+    const b: bigint = initialKeys[1] & BigInt("0xFF");
+    let y: bigint = BigInt(0);
+    for (let j = 0; j < 8; j += 4) {
+      const  abc = Number((b >> BigInt(j)) & BigInt("0xF"));
+      y |= (BigInt(s[abc]) << BigInt(j));
+    }
+    initialKeys[1] = (initialKeys[1] & BigInt("0xFFFFFFFFFFFFFF00") | y );
+    const rc = ((initialKeys[1] >> BigInt(64-5)) & BigInt("0x1F")) ^ BigInt(i);
+    initialKeys[1] = (initialKeys[1] & BigInt("0x07FFFFFFFFFFFFFF")) | (rc << BigInt(64-5));
+    console.log("encrypted - ", initialKeys[0].toString(16), initialKeys[1].toString(16));
+    console.log();
+
+  }
+}
+
+generateKeys();
+
+
 
 // Encrypt the video file
 const encryptedVideoBuffer = encryptBuffer(videoBuffer);
